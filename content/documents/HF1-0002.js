078b146c378615abc863e3ab51c45e094cc49708
Carl Fireman <GalacticHorizon.Florida@proton.me>
2026-09-22 11:36:41 -0500

    HF1#0002

--- content/documents/HF1-0002.js
+++ content/documents/HF1-0002.js
@@ -4,17 +4,17 @@ export const HF1_0002 = {
   dataset: null,
   sortOrder: null,
   title: "HF1#0002",
-  status: "deprecated",
-  date: "",
-  dateDisplay: "",
-  roughTime: "",
-  location: "",
-  people: [],
-  keywords: [],
-  summary: "",
+  status: "Active",
+  date: "December 27, 2026",
+  dateDisplay: "7:15pm CST",
+  roughTime: "Night",
+  location: "Florida",
+  people: ["P-002", "P-011"],
+  keywords: ["Party"],
+  summary: "Image taken days before new years",
   context: `
-
+This photo was from a collection of photos containing Hunter Pita Yancey along with John Doe #2 and an unknown redacted individual. We are currently unaware of the address in which this photo was taken.
 `,
-  relatedDocuments: [],
-  lastUpdated: null,
+  relatedDocuments: ["HF1#0003","HF1#0004"],
+  lastUpdated: 2026-09-22,
 };
